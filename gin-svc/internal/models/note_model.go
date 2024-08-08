46e863fba1b399023eb00738bad1a831b5b3d3d4
linlinWu <18574945291@163.com>
2024-08-08 19:28:16 +0800

    feat: add models

--- gin-svc/internal/models/note_model.go
+++ gin-svc/internal/models/note_model.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type NoteModel struct {
 	gorm.Model
 	Title      string `json:"title" gorm:"column:title;type:varchar(255);comment:笔记主标题"`
+	Cover      string `json:"cover" gorm:"column:cover;type:varchar(255);comment:封面图片URL"`
 	Mark       string `json:"mark" gorm:"column:mark;type:varchar(255);comment:笔记描述"`
 	Type       string `json:"type" gorm:"column:type;type:varchar(8);comment:类型,图片，视频"`
 	LikeCnt    int    `json:"like_cnt" gorm:"column:like_cnt;type:int;comment:点赞数"`
@@ -13,5 +14,5 @@ type NoteModel struct {
 }
 
 func (NoteModel) TableName() string {
-	return "article"
+	return "notes"
 }
---
+++ gin-svc/internal/models/tag_model.go
@@ -0,0 +1,14 @@
+package models
+
+import "gorm.io/gorm"
+
+type TagModel struct {
+	gorm.Model
+	Name        string `json:"name" gorm:"type:varchar(50);not null;unique"`
+	Description string `json:"description" gorm:"type:varchar(255)"`
+	Status      bool   `json:"status" gorm:"type:tinyint(1);default:1"`
+}
+
+func (TagModel) TableName() string {
+	return "tags"
+}
