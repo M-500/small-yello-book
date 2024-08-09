6a42b1dc57401d285acc35f93f3358f8ddd484ca
linlinWu <18574945291@163.com>
2024-08-09 13:12:54 +0800

    add front web

--- gin-svc/internal/models/sys_model.go
+++ gin-svc/internal/models/sys_model.go
@@ -3,6 +3,14 @@ package models
 import "gorm.io/gorm"
 
 type SysPermissionModel struct {
+	gorm.Model
+	Title  string `json:"title" gorm:"column:title;type:varchar(100);not null;default:'';comment:权限名称"`
+	Action string `json:"action" gorm:"column:action;type:varchar(32);not null;default:'';comment:权限动作,对应HTTP请求方式"`
+	PerKey string `json:"perKey" gorm:"unique;column:per_key;type:varchar(64);not null;default:'';comment:权限关键字(唯一索引)"`
+	Mark   string `json:"mark" gorm:"column:mark;type:varchar(100);not null;default:'';comment:权限标识"`
+	Path   string `json:"path" gorm:"column:path;type:varchar(255);not null;default:'';comment:权限路径"`
+	Type   string `json:"type" gorm:"column:type;type:varchar(32);not null;default:'BUS';comment:权限类型 BUS/SYS"`
+	Status bool   `json:"status" gorm:"column:status;type:tinyint(1);not null;default:1;comment:状态,0 禁用，1启用"`
 }
 
 func (SysPermissionModel) TableName() string {
@@ -11,9 +19,12 @@ func (SysPermissionModel) TableName() string {
 
 type SysRoleModel struct {
 	gorm.Model
-	RoleName string `json:"roleName" gorm:"type:varchar(128);not null;comment:角色名称"`
-	RoleKey  string `json:"roleKey" gorm:"type:varchar(128);not null;comment:角色标识"`
-	Sort     int    `json:"sort" gorm:"type:int;not null;comment:排序"`
+	CreatorId int64  `gorm:"not null;type:int;column:creator_id;comment:创建人ID"`
+	RoleName  string `gorm:"not null;type:varchar(32);column:role_name;comment:角色名"`
+	RoleKey   string `gorm:"not null;unique;type:varchar(100);column:role_key;comment:角色字符串(唯一索引,不可更改)"`
+	Status    bool   `gorm:"comment:角色状态"`
+	RoleSort  int    `gorm:"comment:角色排序"`
+	Remark    string `gorm:"type:varchar(100);comment:角色描述信息"`
 }
 
 func (SysRoleModel) TableName() string {
