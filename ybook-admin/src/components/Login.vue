fe15fbaecaa9a75c3cae7b955b8e6f2438903228
linlinWu <18574945291@163.com>
2024-08-12 17:36:57 +0800

    feat: login page

--- ybook-admin/src/components/Login.vue
+++ ybook-admin/src/components/Login.vue
@@ -1,10 +1,25 @@
 <template>
-    <div class="container">
-
+    
+<el-dialog
+    v-model="dialogVisible"
+    title="Tips"
+    width="500"
+    :before-close="handleClose"
+  >
+    <div class="login-container">
+        登陆组件啊傻逼
     </div>
+  </el-dialog>
 </template>
 
 
 
 <script setup>
+import { ref } from 'vue';
+const dialogVisible = ref(false)
+
+function handleClose(done) {
+    dialogVisible.value = false;
+    done();
+}
 </script>
--- ybook-admin/src/components/layout/Aside.vue
+++ ybook-admin/src/components/layout/Aside.vue
@@ -27,14 +27,48 @@
            
         </div>
 
-        
+        <el-dialog
+    v-model="dialogVisible"
+    width="500"
+  >
+    <div class="login_container">
+        <div class="left">
+            <div class="mark">登陆后推荐更懂你的笔记</div>
+            <div class="logo">
+                <img src="../../assets/logo.svg" alt="慕课网">
+            </div>
+            <div class="wechar_code">
+                微信二维码
+            </div>
+        </div>
+
+        <div class="right">
+            <div class="login_title">
+                账号密码登录
+            </div>
+            <div class="login_form">
+                <el-input class="form_item" placeholder="请输入手机号"></el-input>
+                <el-input class="form_item" placeholder="请输入密码"></el-input>
+                <el-button type="primary" class="login_btn">登陆</el-button>
+            </div>
+            <div class="protocl">
+                <el-checkbox></el-checkbox>
+                <a href="#">我已阅读并同意《用户协议》</a>
+            </div>
+
+        </div>
+    </div>
+  </el-dialog>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const dialogVisible: Ref<boolean> = ref(false);
 function loginBtnClick(){
     console.log('click');
+    dialogVisible.value = true;
 }
 </script>
 <style scoped>
@@ -48,6 +82,7 @@ function loginBtnClick(){
     padding-top: 16px;
     /* height: 150; */
 }
+
 
 .active-box{
     background-color: rgba(190, 187, 187, 0.6);
@@ -90,5 +125,59 @@ function loginBtnClick(){
     text-align: center;
     background-color: #f5f5f5;
     border-top: 1px solid #ebebeb;
+}
+
+.login_container{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    height: 100%;
+}
+.logo img{
+    height: 24px;
+}
+.left{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-right: 1px solid #ebebeb;
+}
+
+/deep/.el-dialog{
+    display: flex;
+    position: relative;
+    justify-content: center;
+    width: 800px;
+    height: 480px;
+    border-radius: 16px;
+}
+
+.login_btn{
+    background-color: #ff2e4d;
+    border: #ebebeb;
+    border-radius: 50px;
+    width: 100%;
+    height: 48px;
+    margin-top: 16px;
+}
+/deep/ .login_btn:hover{
+    background-color: #ff2e4d;
+}
+
+.form_item{
+    margin-top: 16px;
+    height: 48px;
+    font-size: 16px;
+    caret-color: #ff2e4d;
+    color: #333;
+}
+
+/deep/ .el-input__wrapper{
+    border-radius: 50px;
+}
+
+/deep/  .is_focus {
+    border: 1px solid #ff2e4d;
 }
 </style>
