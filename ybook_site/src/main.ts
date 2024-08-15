7ed82abd02ed84439096468b1450a0757bb5902c
linlinWu <18574945291@163.com>
2024-08-15 23:17:17 +0800

    feat: ElementUI国际化处理

--- ybook_site/src/main.ts
+++ ybook_site/src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus' // 引入ElementPlus
 import 'element-plus/dist/index.css' // 引入ElementPlus的样式
+import zhCn from 'element-plus/es/locale/lang/zh-cn' // 引入ElementPlus的中文语言包
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus) // 使用ElementPlus
+app.use(ElementPlus, {
+  locale: zhCn // 使用ElementPlus的中文语言包
+}) // 使用ElementPlus
 app.mount('#app')
