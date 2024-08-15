409611eb1b0c5b55196c6aa21c314eed25beddfb
linlinWu <18574945291@163.com>
2024-08-15 23:11:34 +0800

    feat: 集成ElementUI

--- ybook_site/src/main.ts
+++ ybook_site/src/main.ts
@@ -2,7 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus' // 引入ElementPlus
+import 'element-plus/dist/index.css' // 引入ElementPlus的样式
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus) // 使用ElementPlus
 app.mount('#app')
