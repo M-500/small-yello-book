c83e4a5ec46253be013c8734ece729083d7a46a3
linlinWu <18574945291@163.com>
2024-08-15 22:39:17 +0800

    feat:新增前端项目

--- gin-svc/internal/web/middleware/permission_mdl.go
+++ gin-svc/internal/web/middleware/permission_mdl.go
@@ -4,8 +4,21 @@ import "github.com/gin-gonic/gin"
 
 // 权限校验中间件
 
-func PermissionCheckMdl() gin.HandlerFunc {
-	return func(c *gin.Context) {
+type PermissionMiddleware struct {
+	path []string // 不需要权限校验的路径
+}
+
+func NewPermissionMdl() *PermissionMiddleware {
+	return &PermissionMiddleware{}
+}
+
+func (m *PermissionMiddleware) Build() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 
 	}
+}
+
+func (m *PermissionMiddleware) IgnorePath(path string) *PermissionMiddleware {
+	m.path = append(m.path, path)
+	return m
 }
