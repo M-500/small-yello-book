0626bf25a38603963d4e6995512e67bcbf9ee992
linlinWu <18574945291@163.com>
2024-08-20 00:00:52 +0800

    fix: logo组件跳转首页

--- ybook-admin/src/components/Logo/index.vue
+++ ybook-admin/src/components/Logo/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="header-container">
-    <svg-icon name="logo"
+    <svg-icon @click="handleClick"
+              name="logo"
               width="72"
               height="72" />
     <div class="title">创作服务平台</div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+let $router = useRouter()
+function handleClick(){
+	$router.push('/home')
+}
 </script>
 
 <style lang="scss" scoped>
