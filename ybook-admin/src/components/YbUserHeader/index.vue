be138eff80dce62e6f9f6372271a60d6b20be564
linlinWu <18574945291@163.com>
2024-08-19 23:59:24 +0800

    feat: 优化用户信息的模块

---
+++ ybook-admin/src/components/YbUserHeader/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="container">
+    <div class="box"
+         @click="showLogout">
+      <img src="@/assets/imgs/curry.jpeg"
+           alt="">
+      <span class="user-name">巧舌如簧的哑巴</span>
+      <el-icon class="icon">
+        <ArrowDown />
+      </el-icon>
+
+      <div class="menu-list"
+           v-show="showLogoutVisable"
+           @click="open">
+        <div class="popover_text">
+          <el-icon class="logout-icon">
+            <SwitchButton />
+          </el-icon>
+          <span class="inline">退出登录</span>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import useUserStore from '@/stores/moudules/user';
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+let userStore = useUserStore()
+let $router = useRouter()
+const showLogoutVisable = ref(false)
+
+function showLogout(){
+	showLogoutVisable.value = !showLogoutVisable.value
+}
+const open = () => {
+	ElMessageBox.confirm(
+    '确定要退出登录吗？',
+    {
+			customClass: 'message-logout',
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning',
+    }
+  )
+    .then(() => {
+			userStore.userLogout()
+			$router.push('/login')
+      ElMessage({
+        type: 'success',
+        message: '退出登录成功',
+      })
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: '',
+      })
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+	.box{
+		display: flex;
+		justify-content: center;
+		img{
+			width: 28px;
+			height: 28px;
+			border-radius: 100%;
+			margin-right: 4px;
+		}
+		.user-name{
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.icon{
+			height: 100%;
+			margin-left: 7px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.menu-list{
+			position: absolute;
+			width: 164px;
+			right: 40px;
+			top: 50px;
+			padding: 8px 0;
+			background: #fff;
+			box-shadow: 0 2px 8px #e4e6f2;
+			border-radius: 4px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			cursor: pointer;
+			.popover_text{
+				width: 100%;
+				height: 40px;
+				// padding-left: 18px;
+				font-family: PingFang SC;
+				font-style: normal;
+				font-weight: 400;
+				font-size: 14px;
+				line-height: 14px;
+				color: #2d2d2d;
+				display: flex;
+				justify-content: flex-start;
+				align-items: center;
+				.logout-icon{
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					margin-right: 14px;
+					margin-left: 14px;
+				}
+				span{
+					font-weight: 900;
+				}
+			}
+		}
+	}
+}
+</style>
+
+<style >
+/* el-message-box的样式修改只能这么玩 */
+.message-logout{
+	border-radius: 10px !important;
+}
+
+</style>
--- ybook-admin/src/layout/YbHeader/index.vue
+++ ybook-admin/src/layout/YbHeader/index.vue
@@ -4,52 +4,15 @@
       <logo />
     </div>
     <div class="right-info">
-      <el-menu :default-active="activeIndex"
-               class="el-menu-demo"
-               mode="horizontal"
-               :ellipsis="false"
-               @select="handleSelect">
-        <el-sub-menu index="2">
-          <template #title>小红薯AbCs123</template>
-          <el-menu-item index=""
-                        @click="open">退出登录</el-menu-item>
-        </el-sub-menu>
-      </el-menu>
+      <yb-user-header></yb-user-header>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import logo from '@/components/Logo/index.vue'
-import useUserStore from '@/stores/moudules/user';
-import { ElMessage, ElMessageBox } from 'element-plus'
-import { useRouter } from 'vue-router';
-let userStore = useUserStore()
-let $router = useRouter()
-const open = () => {
-	ElMessageBox.confirm(
-    '确定要退出登录吗？',
-    {
-      confirmButtonText: '确定',
-      cancelButtonText: '取消',
-      type: 'warning',
-    }
-  )
-    .then(() => {
-			userStore.userLogout()
-			$router.push('/login')
-      ElMessage({
-        type: 'success',
-        message: '退出登录成功',
-      })
-    })
-    .catch(() => {
-      ElMessage({
-        type: 'info',
-        message: '',
-      })
-    })
-}
+import YbUserHeader from '@/components/YbUserHeader/index.vue'
+
 </script>
 
 <style lang="scss" scoped>
@@ -67,22 +30,13 @@ const open = () => {
 		display: flex;
 		align-items: center;
 	}
-	.el-menu-demo{
-		background-color: transparent;
-		border: none;
-		.el-menu-item{
-			color: #fff;
-		}
-		.el-sub-menu{
-			color: #fff;
-		}
-	}
+	
 }
 ::v-deep(.el-sub-menu__title.el-tooltip__trigger.el-tooltip__trigger):hover{ 
 	color: $primary-buttom-active-colder;
 }
-
-::v-deep(.el-message-box){
-	border-radius: 10px;
+::v-deep(.el-button--primary){
+	background-color: $primary-buttom-active-colder !important;
 }
 </style>
+
