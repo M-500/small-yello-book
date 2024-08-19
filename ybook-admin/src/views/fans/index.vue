ba44478728f924bcccee9ae50f830bea3acb756a
linlinWu <18574945291@163.com>
2024-08-19 15:12:19 +0800

    feat: 新增粉丝页面

--- ybook-admin/src/views/fans/index.vue
+++ ybook-admin/src/views/fans/index.vue
@@ -1,11 +1,73 @@
 <template>
-  <div class="">
-    粉丝数据啦
+  <div class="container">
+    <div class="content">
+      <el-card>
+        <div class="fans-data-container">
+
+          <yb-title title="粉丝数据概览"></yb-title>
+
+        </div>
+      </el-card>
+      <div class="fan-bottom">
+        <el-card>
+          <div class="fan-portrait-container">
+
+            <yb-title title="粉丝画像"></yb-title>
+
+          </div>
+        </el-card>
+      </div>
+
+    </div>
+    <div class="fans-content">
+      <el-card>
+        <div class="fans-active">
+          <yb-title title="我的活跃粉丝"></yb-title>
+        </div>
+      </el-card>
+
+    </div>
   </div>
 </template>
 
 <script setup>
-import { } from "vue"
+import YbTitle from '@/components/YbTitle/index.vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container{
+  height: 100%;
+  width: 100%;
+  display: flex;
+  .content{
+    width: calc(100% - 372px);
+    // padding: 0px 24px 32px;
+    .fans-data-container{
+      padding: 0px 24px 32px;
+    }
+    .fan-bottom{
+      margin-top: 16px;
+      .fan-portrait-container{
+        padding: 0px 24px 32px;
+      }
+    }
+    
+  }
+  .fans-content{
+    margin-left: 16px;
+    width: 372px;
+    .fans-active{
+      padding: 0 24px;
+    }
+  }
+}
+
+::v-deep(.el-card){
+  border-radius: 8px;
+  height: 100%;
+}
+::v-deep(.el-card__body){
+  padding: 0;
+  height: 100%;
+}
+</style>
--- ybook-admin/src/views/notedashboard/index.vue
+++ ybook-admin/src/views/notedashboard/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="">
-    笔记数据啦
+  <div class="container">
+    <el-card>
+      <div class="note-info">
+        <yb-title title="单篇笔记数据"></yb-title>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script setup>
-import { } from "vue"
+import YbTitle from '@/components/YbTitle/index.vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container{
+  height: 100%;
+  
+  .note-info{
+    padding: 0px 24px ;
+    display: flex;
+  }
+}
+
+::v-deep(.el-card){
+  border-radius: 8px;
+  height: 100%;
+}
+::v-deep(.el-card__body){
+  padding: 0;
+  height: 100%;
+}
+</style>
