edc04dca5a8d4e99f2a9d9f2baf73e0fc71ff7a3
linlinWu <18574945291@163.com>
2024-09-24 18:53:24 +0800

    cao

---
+++ ybook-admin/admin/assets/index-BGD2AnaF.js
@@ -0,0 +1,1 @@
+import{A as s}from"./index-BuezEob_.js";const a=t=>s.post("/api/v1/notes",t),o=t=>s.get("/api/v1/notes",{params:t}),p=t=>s.put("/api/v1/notes/:id/pass".replace(":id",t));export{p as a,o as g,a as p};
