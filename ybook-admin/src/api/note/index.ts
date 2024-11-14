4141bd53cb92228b6bfe7ed7963f7af87317c16c
linlinWu <18574945291@163.com>
2024-11-14 13:40:41 +0800

    修改了一部分bug

--- ybook-admin/src/api/note/index.ts
+++ ybook-admin/src/api/note/index.ts
@@ -3,6 +3,7 @@ import type { publishNoteForm, queryNoteListForm } from './type'
 
 enum NoteApi {
   PUBLISH_NOTE = '/api/v1/notes',
+  PUBLISH_NOTE_VIDEO = '/api/v1/notes/video',
   NOTE_LIST = '/api/v1/notes',
   PASS_NOTE = '/api/v1/notes/:id/pass'
 }
@@ -17,4 +18,9 @@ export const getNoteList = (data: queryNoteListForm) => {
 
 export const passNote = (id: string) => {
   return request.put(NoteApi.PASS_NOTE.replace(':id', id))
+}
+
+// 发布视频笔记
+export const publishVideoNote = (data: publishNoteForm) => {
+  return request.post(NoteApi.PUBLISH_NOTE_VIDEO, data)
 }
